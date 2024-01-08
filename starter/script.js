7306f3dd671faa08942a735a049527684e24b511
rachel esteve <rachiraesteve@yahoo.com>
2024-01-08 16:44:40 +0000

    added color changing

--- starter/script.js
+++ starter/script.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
+  var currentHour=dayjs().hour()
+  var colorKey=""
 for (var i=9;i<19;i++){
+  console.log(i)
+  if(currentHour>i){
+    colorKey="past" 
+  }
+  else if(currentHour===i){colorKey="present"}
+  else{
+    colorKey="future"
+  }
   //let row=document.createElement("div")
   let row=$("<div>").addClass("time-block row").attr("id","hour-"+i)
-  console.log(row)
   let hour=$("<div>").addClass("col-3 hour").text(i)
-  let textarea=$("<textarea>").addClass("description col-7")
+  let textarea=$("<textarea>").addClass("description col-7 "+colorKey).val(localStorage.getItem("hour-"+i))
 let button =$("<button>").addClass("col-2 saveBtn").text("save").on("click", function(){
   let saveBtn=$(this)
+  let hour=saveBtn.parent().attr("id")
+let activity=saveBtn.siblings(".description").val()
+localStorage.setItem(hour, activity)
+
 })
   $(".container").append(row.append(hour, textarea, button))
   //row.setAttribute("class", "time-block")
